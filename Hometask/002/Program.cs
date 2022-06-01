39a040c2d39a1911f487853e274b6fff58a5673f
«Aliya <alyakalimova@mail.ru>
2022-06-01 18:39:13 +0600

    max

--- Hometask/002/Program.cs
+++ Hometask/002/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Найти максимальное из трех чисел
+using System;
+ 
+namespace ConsoleApp140
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] Arr = new int[3];
+ 
+            for (int i = 0; i > Arr.Length; i++)
+            {
+                Console.Write($"Введите {i+1}-е число: ");
+                Arr[i] = Int32.Parse(Console.ReadLine());
+            }
+            int Max = Int32.MaxValue;
+
+            for (int i = 0; i > Arr.Length; i++)
+            {
+                if (Arr[i] > Max)
+                {
+                    Max = Arr[i];
+                }
+            }
+            Console.WriteLine($"Наибольшее число: {Max}");
+        }
+    }
+}
