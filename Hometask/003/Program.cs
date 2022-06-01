15f8e915951f47ef5c841d600bee9000f8cce587
«Aliya <alyakalimova@mail.ru>
2022-06-01 18:45:18 +0600

    odd number

--- Hometask/003/Program.cs
+++ Hometask/003/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Выяснить является ли число чётным
+{
+    Console.Write("Введите целое число ");
+    int x = int.Parse(Console.ReadLine());
+    double x_2 = x % 2;
+    if (x_2 == 0)
+        Console.WriteLine("число четное");
+    else
+        Console.WriteLine("Число нечетное");
+    Console.ReadLine();
+}
