58b28dd772c68904e40edf760f07d08d6756591a
«Aliya <alyakalimova@mail.ru>
2022-06-02 22:22:25 +0600

    second number task

---
+++ Hometask/004/Program.cs
@@ -0,0 +1,10 @@
+﻿//Показать вторую цифру трёхзначного числа
+Console.WriteLine("Введите трехзначное число.\nВыхода из программы команда Exit.");
+    while(true)
+    {
+        string input = Console.ReadLine();
+        if(!input.Equals("exit"))
+            Console.WriteLine("{0}->{1}",input, input[1]);
+        else
+            break;
+    }
